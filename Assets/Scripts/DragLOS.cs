0231de135c6bba68d916ee30b00a9b9b262065ba
Jay Patel <46022232+jaypatel-13@users.noreply.github.com>
2020-01-30 12:36:55 +0530

    30/01

---
+++ Assets/Scripts/DragLOS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragLOS : MonoBehaviour
+{
+    void OnMouseDrag() {
+        
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(ray.origin, ray.direction*100f);
+        if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity, (1<<LayerMask.NameToLayer("Ground"))))
+        {
+            transform.position = new Vector3(hitInfo.point.x,hitInfo.point.y + 5f,hitInfo.point.z);
+        }
+    }
+}
